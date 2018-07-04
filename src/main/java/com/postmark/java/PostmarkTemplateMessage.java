7f6446556f28365238cfc7616051ae8df695ae70
Marcus Ling <marcus.ling@moneygo.se>
2018-07-04 16:27:03 +0200

    updated postmarktemplate to handle arrays

--- src/main/java/com/postmark/java/PostmarkTemplateMessage.java
+++ src/main/java/com/postmark/java/PostmarkTemplateMessage.java
@@ -1,18 +1,23 @@
 package com.postmark.java;
 
+import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import com.google.gson.annotations.SerializedName;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by marcus on 2016-02-02.
  */
 public class PostmarkTemplateMessage extends AbstractPostMarkMessage {
+
     @SkipMe
-    private HashMap<String,HashMap<String,String>> modelParameters = new HashMap<>();
+    private HashMap<String, Object> modelParameters = new HashMap<>();
 
     @SerializedName("TemplateId")
     private int templateId;
@@ -21,7 +26,7 @@ public class PostmarkTemplateMessage extends AbstractPostMarkMessage {
     private JsonElement templateModel;
 
     public PostmarkTemplateMessage(int templateId, String fromAddress, String toAddress, String replyToAddress, String ccAddress, String tag, boolean tracking) {
-        this(templateId,fromAddress,toAddress,replyToAddress,ccAddress,tag,null,tracking);
+        this(templateId, fromAddress, toAddress, replyToAddress, ccAddress, tag, null, tracking);
     }
 
     public PostmarkTemplateMessage(int templateId, String fromAddress, String toAddress, String replyToAddress, String ccAddress, String tag, List<NameValuePair> headers, boolean tracking) {
@@ -35,85 +40,74 @@ public class PostmarkTemplateMessage extends AbstractPostMarkMessage {
         this.tracking = tracking;
     }
 
-
     public void validate() throws PostmarkException {
-
         super.validate();
 
         templateModel = new JsonParser().parse(generateTempleteModel());
-
         if (this.templateModel == null) {
             throw new PostmarkException("You must specify a valid 'To' email address.");
         }
-
     }
 
+    private String generateTempleteModel() {
+        JsonObject entryObject = new JsonObject();
+        generateDataForLevel(entryObject, modelParameters);
+        return entryObject.toString();
+    }
 
-    private String generateTempleteModel(){
-        JsonObject templeteModel = new JsonObject();
-        for(Map.Entry<String,HashMap<String,String>> entry:modelParameters.entrySet()){
-            JsonObject object = new JsonObject();
-            HashMap<String,String> map = entry.getValue();
+    private void generateDataForLevel(JsonObject topObject, HashMap<String, Object> objects) {
+        HashMap<String, HashMap<String, Object>> tree = new HashMap<>();
 
-            for(Map.Entry<String,String> innerEntry: map.entrySet()){
-                String[] entryMap = innerEntry.getKey().split("\\.");
-                if(entryMap.length > 1){
-                    object.add(entryMap[0],createJsonObject(removeFirstInArray(entryMap),innerEntry.getValue()));
-                }else{
-                    object.addProperty(entryMap[0],innerEntry.getValue());
+        for (Map.Entry<String, Object> entry : objects.entrySet()) {
+            String[] map = entry.getKey().split("\\.");
+
+            if (map.length > 1) {
+
+                if (!tree.containsKey(map[0])) {
+                    tree.put(map[0], new HashMap<String, Object>());
+                }
+
+                tree.get(map[0]).put(reformKey(map), entry.getValue());
+
+            } else {
+                if (entry.getValue() instanceof String) {
+                    topObject.addProperty(map[0], (String) entry.getValue());
+                } else {
+                    topObject.add(map[0], (JsonArray) entry.getValue());
                 }
             }
-
-            templeteModel.add(entry.getKey(),object);
         }
 
-        return templeteModel.toString();
+        for (Map.Entry<String, HashMap<String, Object>> entry : tree.entrySet()) {
+
+            if (!topObject.has(entry.getKey())) {
+                topObject.add(entry.getKey(), new JsonObject());
+            }
+            generateDataForLevel(topObject.getAsJsonObject(entry.getKey()), entry.getValue());
+        }
     }
 
-    private JsonObject createJsonObject(String[] entries,String value){
-        String currentEntery = entries[0];
-        JsonObject object = new JsonObject();
-        entries =removeFirstInArray(entries);
-        if(entries.length > 0){
-            object.add(currentEntery,createJsonObject(entries,value));
-        }else{
-            object.addProperty(currentEntery,value);
-        }
-        return object;
-    }
+    private String reformKey(String[] array) {
+        StringBuilder builder = new StringBuilder();
 
-    private String[] removeFirstInArray(String[] array){
-        String[] newArray = new String[array.length-1];
-        for(int i =1; i < array.length; i++){
-            newArray[i-1] = array[i];
-        }
-        return newArray;
-    }
+        for (int i = 1; i < array.length; i++) {
+            builder.append(array[i]);
 
-    public void addParameter(String parameter, String value){
-        String[] map = parameter.split("\\.");
-        String entryKey = map[0];
-        HashMap<String,String> entry;
-
-        if(modelParameters.containsKey(entryKey)){
-            entry = modelParameters.get(entryKey);
-        }else{
-            entry = new HashMap<>();
-        }
-
-        StringBuilder innerEntry = new StringBuilder();
-
-        for (int i= 1; i < map.length;i ++){
-            if(i != map.length){
-                innerEntry.append(map[i]+".");
+            if ((i + 1 != array.length)) {
+                builder.append(".");
             }
         }
 
-        entry.put(innerEntry.toString(),value);
-
-        modelParameters.put(entryKey,entry);
+        return builder.toString();
     }
 
+    public void addParameter(String parameter, String value) {
+        modelParameters.put(parameter, value);
+    }
+
+    public void addParameter(String parameter, JsonArray value) {
+        modelParameters.put(parameter, value);
+    }
 
     public int getTemplateId() {
         return templateId;
